42f5d81fd831491b92dcde4887cc7a1db61064ea
Julian Bristol <bristoljulian.r@gmail.com>
2023-05-19 23:40:05 -0400

    Add Missing Page handling

--- client/src/components/Projects/Project.jsx
+++ client/src/components/Projects/Project.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import posts from '../../posts.json'
 import Posts from '../Posts/Posts';
@@ -6,10 +6,13 @@ import Posts from '../Posts/Posts';
 const Project = () => {
   const { id } = useParams();
   const project = posts.find(obj => obj.id === id);
-
+  
   return (
     <>
-      <Posts singlePost={true} post={[project]}/>
+      {project === undefined ? 
+      <>Error 404: Project Not Found</>
+      : 
+      <Posts singlePost={true} post={[project]}/>}
     </>
 )
 }
