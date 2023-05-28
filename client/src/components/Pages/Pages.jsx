6f7912625009d54e7cdb71060390b4d771492fb6
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-05-28 17:31:22 -0400

    Add Pages template

--- client/src/components/Pages/Pages.jsx
+++ client/src/components/Pages/Pages.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import pages from '../../pages.json';
+import { Avatar, Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
 
 const Pages = () => {
   return (
     <div>
-      This page will contain a list of pages
+      {pages.map((page, key) => (
+        <Card key={key} sx={{ height: '400px', width: '400px' }}>
+          <CardMedia sx={{ height: '200px', width: '400px' }}>
+            <img
+            style={{ height: '200px', width: '400px', objectFit: 'cover' }}
+            src={"https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg"}
+            alt="testing"
+            />
+          </CardMedia>
+          <CardContent>
+            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+            <Avatar
+              src={"https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg"}
+              alt={"imagessdfs"}
+              sx={{ width: '75px', height: '75px'}}
+            />
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <Typography>Page Name</Typography>
+              <Typography>Type of Page</Typography>
+            </Box>
+            </Box>
+            <Typography>
+            Friend A, Friend B, Friend C, Friend D
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   )
 }
